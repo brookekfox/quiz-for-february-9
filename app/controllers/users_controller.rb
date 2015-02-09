2119db561cf793aad96d362ae04456d7b032d259
brookekfox <brookekfox@gmail.com>
2015-02-09 09:41:55 -0800

    with some css and edit/delete functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,27 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params.require(:user).permit(:username, :email, :birthdate))
+			redirect_to users_path
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to users_path
+	end
+
 end
